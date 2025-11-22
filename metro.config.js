e2526b4c5d8fdd80925b747766671ea83207136b
stuartepworth1-web <stuartepworth1@gmail.com>
2025-11-22 19:44:16 +0800

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -5,12 +5,13 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (moduleName.startsWith('@/')) {
-    const modPath = moduleName.replace('@/', '');
-    return context.resolveRequest(
-      context,
-      path.resolve(__dirname, modPath),
-      platform
-    );
+    const modPath = path.resolve(__dirname, moduleName.replace('@/', ''));
+    try {
+      return context.resolveRequest(context, modPath, platform);
+    } catch (error) {
+      // Fallback to default resolver
+      return context.resolveRequest(context, moduleName, platform);
+    }
   }
   return context.resolveRequest(context, moduleName, platform);
 };
