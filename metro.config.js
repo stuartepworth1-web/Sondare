571de0bbdb52fb8e80b7c81845c59b954f03e51e
stuartepworth1-web <stuartepworth1@gmail.com>
2025-12-05 21:53:25 +0800

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,7 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName.startsWith('@/')) {
-    const modPath = path.resolve(__dirname, moduleName.replace('@/', ''));
-    try {
-      return context.resolveRequest(context, modPath, platform);
-    } catch (error) {
-      // Fallback to default resolver
-      return context.resolveRequest(context, moduleName, platform);
-    }
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
 
 module.exports = config;
